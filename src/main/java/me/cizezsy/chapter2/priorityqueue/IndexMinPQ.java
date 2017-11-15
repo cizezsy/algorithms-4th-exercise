5b28b26c573c2a46e963548ac18f5818b4eb0773
zsy <zsy19980307@gmail.com>
2017-11-15 20:35:21 +0800

    completed to 2.4.34

--- src/main/java/me/cizezsy/chapter2/priorityqueue/IndexMinPQ.java
+++ src/main/java/me/cizezsy/chapter2/priorityqueue/IndexMinPQ.java
@@ -1,39 +1,110 @@
 package me.cizezsy.chapter2.priorityqueue;
 
-public class IndexMinPQ<Item extends Comparable<Item>> {
+public class IndexMinPQ<Key extends Comparable<Key>> {
 
-//    public IndexMinPQ(int maxN) {
-//
-//    }
-//
-//    public void insert(int k, Item item) {
-//
-//    }
-//
-//    public void change(int k, Item item) {
-//
-//    }
-//
-//    public boolean contains(int k) {
-//
-//    }
-//
-//    public void delete(int k) {
-//    }
-//
-//    public Item min() {
-//
-//    }
-//
-//    public int delMin() {
-//
-//    }
-//
-//    public boolean isEmpty() {
-//
-//    }
-//
-//    public int size() {
-//
-//    }
+    private int N = 0;
+    private int[] pq;
+    private int[] qp;
+    private Key[] keys;
+
+    public IndexMinPQ(int maxN) {
+        keys = (Key[]) new Comparable[maxN + 1];
+        pq = new int[maxN + 1];
+        qp = new int[maxN + 1];
+        for (int i = 0; i <= maxN; i++) qp[i] = -1;
+    }
+
+    public void insert(int k, Key key) {
+        N++;
+        qp[k] = N;
+        pq[N] = k;
+        keys[k] = key;
+        swim(N);
+    }
+
+    private void swim(int k) {
+        while (k > 1 && less(k, k / 2)) {
+            exch(k / 2, k);
+            k = k / 2;
+        }
+    }
+
+    private void sink(int k) {
+        while (k * 2 <= N) {
+            int j = 2 * k;
+            if (j < N && less(j + 1, j)) j++;
+            if (!less(j, k)) break;
+            exch(k, j);
+            k = j;
+        }
+    }
+
+
+    public boolean contains(int k) {
+        return qp[k] != -1;
+    }
+
+    public void change(int k, Key key) {
+        keys[k] = key;
+        swim(qp[k]);
+        sink(qp[k]);
+    }
+
+
+    public void delete(int k) {
+        exch(qp[k], N--);
+        swim(qp[k]);
+        sink(qp[k]);
+        keys[pq[N + 1]] = null;
+        qp[pq[N + 1]] = -1;
+    }
+
+    public Key min() {
+        return keys[pq[1]];
+    }
+
+    public int minIndex() {
+        return pq[1];
+    }
+
+    public int delMin() {
+        int indexOfMin = pq[1];
+        exch(indexOfMin, N--);
+        sink(1);
+        keys[pq[N + 1]] = null;
+        qp[pq[N + 1]] = -1;
+        return indexOfMin;
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    private boolean less(int i, int j) {
+        return keys[pq[i]].compareTo(keys[pq[j]]) < 0;
+    }
+
+    private void exch(int i, int j) {
+        int t = pq[i];
+        pq[i] = pq[j];
+        pq[j] = t;
+        qp[pq[i]] = i;
+        qp[pq[j]] = j;
+    }
+
+    public static void main(String[] args) {
+        IndexMinPQ<String> indexMinPQ = new IndexMinPQ<>(20);
+        indexMinPQ.insert(1, "4");
+        indexMinPQ.insert(2, "3");
+        indexMinPQ.insert(3, "2");
+        indexMinPQ.insert(4, "1");
+
+        System.out.println(indexMinPQ.delMin());
+    }
+
+
 }
